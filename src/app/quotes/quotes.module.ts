d5a193fb535b5529bc9848071c4fd44dbc83bac4
Daniel Ko <danko56666@gmail.com>
2019-09-04 13:48:03 -0400

    quotes page ui

--- src/app/quotes/quotes.module.ts
+++ src/app/quotes/quotes.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { QuotesRoutingModule } from './quotes-routing.module';
-import { QuotesComponent } from './quotes.component';
-
+import { QuotesRoutingModule } from "./quotes-routing.module";
+import { QuotesComponent } from "./quotes.component";
+import { MaterialModule } from "../material.module";
 
 @NgModule({
   declarations: [QuotesComponent],
-  imports: [
-    CommonModule,
-    QuotesRoutingModule
-  ]
+  imports: [CommonModule, QuotesRoutingModule, MaterialModule]
 })
-export class QuotesModule { }
+export class QuotesModule {}
